e4b3f486589b3c5bd58899166f48f873f5c4db10
Vladimir Stebakov <stebakovdevelop@gmail.com>
2022-03-27 15:41:23 +0300

    added base logic

---
+++ app/src/main/java/com/stebakov/holybible/data/BookData.kt
@@ -0,0 +1,8 @@
+package com.stebakov.holybible.data
+
+import com.stebakov.holybible.core.Abstract
+import com.stebakov.holybible.domain.BookDomain
+
+sealed class BookData : Abstract.Object<BookDomain, Abstract.Mapper.Empty>() {
+    //todo fix
+}
---
+++ app/src/main/java/com/stebakov/holybible/domain/BookDomain.kt
@@ -0,0 +1,8 @@
+package com.stebakov.holybible.domain
+
+import com.stebakov.holybible.core.Abstract
+import com.stebakov.holybible.presentation.BookUi
+
+sealed class BookDomain : Abstract.Object<BookUi, Abstract.Mapper.Empty>() {
+    //todo fix
+}
